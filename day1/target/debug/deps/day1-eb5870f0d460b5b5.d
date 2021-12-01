383bf531b3d36883b18f0ae3f225127aec0b57bf
Jason Larsen <jason.larsen@gmail.com>
2021-11-30 22:28:27 -0700

    generate day1

---
+++ day1/target/debug/deps/day1-eb5870f0d460b5b5.d
@@ -0,0 +1,5 @@
+/Users/jarsen/Code/aoc21/day1/target/debug/deps/day1-eb5870f0d460b5b5.rmeta: src/main.rs
+
+/Users/jarsen/Code/aoc21/day1/target/debug/deps/day1-eb5870f0d460b5b5.d: src/main.rs
+
+src/main.rs:
---
+++ day1/target/debug/deps/day1-f0098721b5675f48.d
@@ -0,0 +1,5 @@
+/Users/jarsen/Code/aoc21/day1/target/debug/deps/day1-f0098721b5675f48.rmeta: src/main.rs
+
+/Users/jarsen/Code/aoc21/day1/target/debug/deps/day1-f0098721b5675f48.d: src/main.rs
+
+src/main.rs:
