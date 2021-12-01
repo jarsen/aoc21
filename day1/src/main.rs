2a4b11fe8d7a9b3542d21a1dfcacbf7f4cd6cf35
Jason Larsen <jason.larsen@gmail.com>
2021-12-01 10:21:16 -0700

    borrow correctly in part1/part2 functions

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,28 +3,23 @@ use std::fs;
 fn main() {
     let path = std::env::args().nth(1).expect("no path given");
 
-    // part1(path);
-    part2(path);
+    part1(&path);
+    part2(&path);
 }
 
-fn part1(path: String) {
+fn part1(path: &String) {
     let quickness = fs::read_to_string(path)
         .expect("Something went wrong reading the file")
         .lines()
         .map(|line| line.parse::<i32>().unwrap())
         .collect::<Vec<i32>>()
         .windows(2)
-        .fold(0, |count, chunk| {
-            if chunk[0] < chunk[1] {
-                count + 1
-            } else {
-                count
-            }
-        });
+        .filter(|window| window[0] < window[1])
+        .count();
     println!("{}", quickness);
 }
 
-fn part2(path: String) {
+fn part2(path: &String) {
     let quickness = fs::read_to_string(path)
         .expect("Something went wrong reading the file")
         .lines()
@@ -34,12 +29,7 @@ fn part2(path: String) {
         .map(|window| window.iter().sum())
         .collect::<Vec<i32>>()
         .windows(2)
-        .fold(0, |count, chunk| {
-            if chunk[0] < chunk[1] {
-                count + 1
-            } else {
-                count
-            }
-        });
+        .filter(|window| window[0] < window[1])
+        .count();
     println!("{}", quickness);
 }
