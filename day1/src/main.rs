e27b2f9b251ede4013010691ac579a8b9e7bfcbd
Jason Larsen <jason.larsen@gmail.com>
2021-12-01 10:30:57 -0700

    extract read_depths

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,29 +2,29 @@ use std::fs;
 
 fn main() {
     let path = std::env::args().nth(1).expect("no path given");
-
-    part1(&path);
-    part2(&path);
+    let depths = read_depths(&path);
+    part1(&depths);
+    part2(&depths);
 }
 
-fn part1(path: &String) {
-    let quickness = fs::read_to_string(path)
+fn read_depths(path: &String) -> Vec<i32> {
+    fs::read_to_string(path)
         .expect("Something went wrong reading the file")
         .lines()
         .map(|line| line.parse::<i32>().unwrap())
         .collect::<Vec<i32>>()
+}
+
+fn part1(depths: &Vec<i32>) {
+    let quickness = depths
         .windows(2)
         .filter(|window| window[0] < window[1])
         .count();
     println!("{}", quickness);
 }
 
-fn part2(path: &String) {
-    let quickness = fs::read_to_string(path)
-        .expect("Something went wrong reading the file")
-        .lines()
-        .map(|line| line.parse::<i32>().unwrap())
-        .collect::<Vec<i32>>()
+fn part2(depths: &Vec<i32>) {
+    let quickness = depths
         .windows(3)
         .map(|window| window.iter().sum())
         .collect::<Vec<i32>>()
