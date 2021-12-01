f1178f194bae5eabeed23409490a4a5d37a46f84
Jason Larsen <jason.larsen@gmail.com>
2021-12-01 10:18:50 -0700

    finish day1 part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,7 +3,8 @@ use std::fs;
 fn main() {
     let path = std::env::args().nth(1).expect("no path given");
 
-    part1(path);
+    // part1(path);
+    part2(path);
 }
 
 fn part1(path: String) {
@@ -11,6 +12,26 @@ fn part1(path: String) {
         .expect("Something went wrong reading the file")
         .lines()
         .map(|line| line.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>()
+        .windows(2)
+        .fold(0, |count, chunk| {
+            if chunk[0] < chunk[1] {
+                count + 1
+            } else {
+                count
+            }
+        });
+    println!("{}", quickness);
+}
+
+fn part2(path: String) {
+    let quickness = fs::read_to_string(path)
+        .expect("Something went wrong reading the file")
+        .lines()
+        .map(|line| line.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>()
+        .windows(3)
+        .map(|window| window.iter().sum())
         .collect::<Vec<i32>>()
         .windows(2)
         .fold(0, |count, chunk| {
