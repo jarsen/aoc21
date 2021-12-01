3a6937f5e9b22ff360773ca068cfb7f2ac956f0d
Jason Larsen <jason.larsen@gmail.com>
2021-11-30 22:53:16 -0700

    initial test solution with harcoded input

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let numbers = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
+    let quickness = numbers.windows(2).fold(0, |count, chunk| {
+        if chunk[0] < chunk[1] {
+            count + 1
+        } else {
+            count
+        }
+    });
+    println!("{}", quickness);
 }
