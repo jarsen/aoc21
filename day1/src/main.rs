85cde4c44163fd0bfbb35073657aff608f0a184c
Jason Larsen <jason.larsen@gmail.com>
2021-11-30 22:59:20 -0700

    use args to get path

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
-    let numbers = get_numbers(String::new());
+    let path = std::env::args().nth(1).expect("no path given");
+    let numbers = get_numbers(path);
 
     let quickness = numbers.windows(2).fold(0, |count, chunk| {
         if chunk[0] < chunk[1] {
@@ -11,7 +12,7 @@ fn main() {
     println!("{}", quickness);
 }
 
-fn get_numbers(_file_name: String) -> [i32; 10] {
+fn get_numbers(_path: String) -> [i32; 10] {
     let numbers = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
     numbers
 }
