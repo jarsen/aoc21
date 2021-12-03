9851b26178ec6cca7a045f91b135e96aa9cf2bde
Jason Larsen <jason.larsen@gmail.com>
2021-12-03 09:13:47 -0700

    infer epsilon from gamma

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,28 +1,16 @@
-struct PowerReading {
-    gamma: u32,
-    epsilon: u32,
-}
-
-impl PowerReading {
-    fn consumption(&self) -> u32 {
-        self.gamma * self.epsilon
-    }
+fn consumption(gamma: u32) -> u32 {
+    let mask = (1 << 5) - 1;
+    let episilon = !gamma & mask;
+    gamma * episilon
 }
 
 #[cfg(test)]
-mod power_reading_tests {
-    use crate::PowerReading;
+mod consumption_tests {
+    use crate::consumption;
 
     #[test]
     fn calculates_consumption() {
-        assert_eq!(
-            PowerReading {
-                gamma: 22,
-                epsilon: 9
-            }
-            .consumption(),
-            198
-        );
+        assert_eq!(consumption(22), 198);
     }
 }
 
