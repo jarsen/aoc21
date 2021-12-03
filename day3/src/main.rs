9bf8d7151ce7c57f15fd199530aa1db2c34e2ff5
Jason Larsen <jason.larsen@gmail.com>
2021-12-03 13:33:00 -0700

    move consumption into GammaReader

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,21 +1,5 @@
 use std::fs;
 
-fn consumption(gamma: u32) -> u32 {
-    let mask = (1 << 5) - 1;
-    let episilon = !gamma & mask;
-    gamma * episilon
-}
-
-#[cfg(test)]
-mod consumption_tests {
-    use crate::consumption;
-
-    #[test]
-    fn calculates_consumption() {
-        assert_eq!(consumption(22), 198);
-    }
-}
-
 struct GammaReader {
     num_readings: u64,
     one_counts: [u64; 5],
@@ -49,12 +33,21 @@ impl GammaReader {
         }
         gamma
     }
+
+    fn consumption(&self) -> u64 {
+        let gamma = self.calc_gamma();
+        let mask = (1 << 5) - 1;
+        let episilon = !gamma & mask;
+        gamma * episilon
+    }
 }
 
 #[cfg(test)]
 mod gamma_reader_tests {
     use crate::GammaReader;
-    static INPUT: &str = r#"00100
+
+    fn read_test_input(reader: &mut GammaReader) {
+        let input = r#"00100
 11110
 10110
 10111
@@ -66,13 +59,16 @@ mod gamma_reader_tests {
 11001
 00010
 01010"#;
+        for reading in input.lines() {
+            reader.add_reading(reading);
+        }
+    }
 
     #[test]
     fn it_reads() {
         let reader = &mut GammaReader::new();
-        for reading in INPUT.lines() {
-            reader.add_reading(reading);
-        }
+        read_test_input(reader);
+
         assert_eq!(reader.num_readings, 12);
         assert_eq!(reader.one_counts[0], 7);
         assert_eq!(reader.one_counts[1], 5);
@@ -81,10 +77,17 @@ mod gamma_reader_tests {
     #[test]
     fn it_calculates_gamma() {
         let reader = &mut GammaReader::new();
-        for reading in INPUT.lines() {
-            reader.add_reading(reading);
-        }
+        read_test_input(reader);
+
         assert_eq!(reader.calc_gamma(), 22);
+    }
+
+    #[test]
+    fn it_calculates_consumption() {
+        let reader = &mut GammaReader::new();
+        read_test_input(reader);
+
+        assert_eq!(reader.consumption(), 198);
     }
 }
 
