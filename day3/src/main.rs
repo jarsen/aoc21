bb5c429f0996d953a0662c45464fcfbf4d898504
Jason Larsen <jason.larsen@gmail.com>
2021-12-03 09:02:57 -0700

    initital consumption calc

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -3,6 +3,29 @@ struct PowerReading {
     epsilon: u32,
 }
 
+impl PowerReading {
+    fn consumption(&self) -> u32 {
+        self.gamma * self.epsilon
+    }
+}
+
+#[cfg(test)]
+mod power_reading_tests {
+    use crate::PowerReading;
+
+    #[test]
+    fn calculates_consumption() {
+        assert_eq!(
+            PowerReading {
+                gamma: 22,
+                epsilon: 9
+            }
+            .consumption(),
+            198
+        );
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
